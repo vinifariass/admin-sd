c4beadb582d06126df0394748bae3d93efd3ae84
Vinicius Farias <fsvinicius9@gmail.com>
2025-03-30 00:32:21 -0300

    Fix navbar in root

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,6 +4,7 @@ import UserButton from "@/components/shared/header/user-button";
 import Link from "next/link";
 import Image from 'next/image';
 import AdminPanelLayout from "@/components/admin-panel/admin-panel-layout";
+import { SheetMenu } from "@/components/admin-panel/sheet-menu";
 
 export default function RootLayout({
     children,
@@ -12,8 +13,23 @@ export default function RootLayout({
 }>) {
     return (
         <>
-            <AdminPanelLayout>{children}
-
+            <AdminPanelLayout>
+            <header className="border-b container mx-auto flex items-center h-16 px-4">
+                <SheetMenu />
+                {/* Logo */}
+                <Link href="/" className="w-22">
+                    <Image src="/images/logo.png" height={48} width={48} alt="Logo" />
+                </Link>
+                {/* Botão da Conta */}
+                <div className="ml-auto items-center flex space-x-4">
+                    <nav className="hidden md:flex w-full max-w-xs gap-1">
+                        <UserButton />
+                    </nav>
+                </div>
+            </header>
+            <div className="flex-1 p-8 pt-6 container mx-auto overflow-y-auto">
+                {children}
+            </div>
             </AdminPanelLayout>
 
 
